acb60bab255bc4e42fb53c9f6d3b997e3aeb4e50
Chousour <99237596+Chousour@users.noreply.github.com>
2022-02-15 12:04:15 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Name: Leok Chhousour" << endl;
+    cout << "Age: 18" << endl;
+    cout << "Phone Number: 087534714" << endl;
+    return 0;
+}
